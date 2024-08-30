13b345d15df48419b56e917853473b917117c0f2
Atlassian <github@atlassian.com>
2024-08-30 11:37:02 +0000

    Synchronise latest changes

--- packages/react-accessibility/src/drag-handle-button-small.tsx
+++ packages/react-accessibility/src/drag-handle-button-small.tsx
@@ -1,6 +1,8 @@
 import React, { forwardRef } from 'react';
 
 import DragHandlerIcon from '@atlaskit/icon/glyph/drag-handler';
+import DragHandlerIconNew from '@atlaskit/icon/utility/drag-handle';
+import { fg } from '@atlaskit/platform-feature-flags';
 import { Box, xcss } from '@atlaskit/primitives';
 
 import { DragHandleButtonBase } from './drag-handle-button-base';
@@ -9,6 +11,10 @@ import type { DragHandleButtonProps } from './types';
 const iconSmallStyles = xcss({
 	display: 'inline-flex',
 	marginInline: 'space.negative.050',
+});
+
+const iconSmallStylesNew = xcss({
+	display: 'inline-flex',
 });
 
 /**
@@ -21,9 +27,13 @@ export const DragHandleButtonSmall = forwardRef<HTMLButtonElement, DragHandleBut
 	function DragHandleButton({ label, ...buttonProps }, ref) {
 		return (
 			<DragHandleButtonBase ref={ref} {...buttonProps}>
-				<Box xcss={iconSmallStyles}>
+				<Box xcss={fg('platform-component-visual-refresh') ? iconSmallStylesNew : iconSmallStyles}>
 					{/* Relying on currentColor for color */}
-					<DragHandlerIcon label={label} size="small" />
+					{fg('platform-component-visual-refresh') ? (
+						<DragHandlerIconNew color="currentColor" label={label} />
+					) : (
+						<DragHandlerIcon label={label} size="small" />
+					)}
 				</Box>
 			</DragHandleButtonBase>
 		);

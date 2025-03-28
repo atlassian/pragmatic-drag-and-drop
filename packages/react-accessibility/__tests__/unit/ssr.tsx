c86481718cb9a86fa7bf0656d72c4d4d92104910
Atlassian <github@atlassian.com>
2025-03-28 11:39:03 +0000

    Synchronise latest changes

--- packages/react-accessibility/__tests__/unit/ssr.tsx
+++ packages/react-accessibility/__tests__/unit/ssr.tsx
@@ -1,33 +1,9 @@
-import { screen } from '@testing-library/react';
+import React from 'react';
 
-import { cleanup, hydrate, ssr } from '@atlaskit/ssr/emotion';
+import { doesHydrateWithSsr } from '@atlassian/ssr-tests';
 
-function noop() {}
+import Example from '../../examples/drag-handle-button';
 
-jest.spyOn(global.console, 'error').mockImplementation(noop);
-
-const examplePath = require.resolve('../../examples/drag-handle-button.tsx');
-
-afterEach(() => {
-	// Check cleanup
-	cleanup();
-	// reset mocks
-	jest.resetAllMocks();
-});
-
-// Skipped due to HOT-111922 Fails for React 18
-test.skip('should ssr then hydrate correctly', async () => {
-	const elem = document.createElement('div');
-	const { html, styles } = await ssr(examplePath);
-
-	elem.innerHTML = html;
-	hydrate(examplePath, elem, styles);
-
-	// Jest 29 - Added assertion to fix: Jest worker encountered 4 child process exceptions, exceeding retry limit
-	await screen.findAllByRole('button');
-
-	// No other errors from e.g. hydrate
-	// eslint-disable-next-line no-console
-	const mockCalls = (console.error as jest.Mock).mock.calls;
-	expect(mockCalls.length).toBe(0);
+test('should ssr then hydrate correctly', async () => {
+	expect(await doesHydrateWithSsr(<Example />)).toBe(true);
 });
